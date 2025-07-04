d48ed9483c0b1bc9f285a39637fe5c4e2b7b43cc
vibhuu22 <111479065+vibhuu22@users.noreply.github.com>
2025-07-04 18:29:48 +0530

    Create 3397-FindTheIntegerAddedToArrayI

---
+++ 3397-FindTheIntegerAddedToArrayI/3397-FindTheIntegerAddedToArrayI.cpp
@@ -0,0 +1,9 @@
+// Last updated: 7/4/2025, 6:29:48 PM
+class Solution {
+public:
+    int addedInteger(vector<int>& nums1, vector<int>& nums2) {
+        sort(nums1.begin(),nums1.end());
+        sort(nums2.begin(),nums2.end());
+        return (nums2[0]-nums1[0]);
+    }
+};
