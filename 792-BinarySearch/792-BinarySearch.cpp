1feb57fb366ea8ff1205a26f693b819b23d73329
vibhuu22 <111479065+vibhuu22@users.noreply.github.com>
2025-07-04 18:30:40 +0530

    Create 792-BinarySearch

---
+++ 792-BinarySearch/792-BinarySearch.cpp
@@ -0,0 +1,35 @@
+// Last updated: 7/4/2025, 6:30:40 PM
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int lo = 0 ;
+        int hi = nums.size()-1 ;
+
+        while(lo<=hi)
+        {
+            int mid = lo + (hi-lo)/2 ;
+            if(nums[mid]==target)
+            {
+                return mid ;
+            }
+            else
+            {
+                if (nums[mid]>target)
+                {
+                    hi = mid-1 ;
+                }
+                else
+                {
+                    lo = mid + 1 ;
+                }
+            }
+     
+        }
+
+        return -1 ;
+    }
+
+
+
+    
+};
